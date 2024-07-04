0f8955b3ca4701cd9a54192a9d64b79e8cf8c60c
Khant Hmue <khanthmu152uit@gmail.com>
2024-07-05 00:40:18 +0630

    Integration Spring Boot Backend with React Frontend

--- src/main/java/com/example/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -3,16 +3,18 @@ package com.example.ecommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 import com.example.ecommerce.service.UserService;
 
@@ -25,25 +27,39 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception{
         return http
-            .csrf(AbstractHttpConfigurer::disable)
+            .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(reg -> {
                 reg.requestMatchers("/", "/user/**", "/login").permitAll();
-                reg.requestMatchers("/api/**").hasRole("ADMIN");
-                reg.anyRequest().authenticated();
+                // reg.requestMatchers("/api/**").hasRole("ADMIN");
+                reg.anyRequest().permitAll();
             })
             // .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
             .formLogin(fl -> {
-                fl
-                    .loginPage("/login")
-                    .successHandler(new AuthenticationSuccessHandler())
-                    .permitAll();
+                // fl
+                //     .loginPage("/login")
+                //     // .successHandler(new AuthenticationSuccessHandler())
+                //     .permitAll();
+
+                fl.disable();
             })
             .build();
     }
 
-    @Bean 
+    @Bean
     public UserDetailsService userDetailsService() {
         return userService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
@@ -53,6 +69,7 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/com/example/ecommerce/restController/UserController.java
+++ src/main/java/com/example/ecommerce/restController/UserController.java
@@ -1,13 +1,20 @@
 package com.example.ecommerce.restController;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ecommerce.dto.AuthRequestDto;
 import com.example.ecommerce.dto.UserDto;
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.service.UserCRUDService;
@@ -16,9 +23,30 @@ import com.example.ecommerce.service.UserCRUDService;
 @RequestMapping("/user")
 public class UserController {
     private final UserCRUDService userCRUDService;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(UserCRUDService userCRUDService) {
+    public UserController(
+        UserCRUDService userCRUDService,
+        AuthenticationManager authenticationManager
+    ) {
         this.userCRUDService = userCRUDService;
+        this.authenticationManager = authenticationManager;   
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequestDto authRequestDto) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    authRequestDto.getUsername(),
+                    authRequestDto.getPassword()
+                )
+            );
+
+            return ResponseEntity.ok(authentication.getPrincipal());
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid Username or password"));
+        }
     }
 
     @PostMapping("/register")
